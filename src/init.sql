7bba48ed6e320d3bcf7b7eb8b5de2b12318acb57
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-23 22:07:46 +0300

    Tables create statement added.

---
+++ src/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id 			INTEGER PRIMARY KEY AUTOINCREMENT,
+    username 	TEXT NOT NULL,
+    email 		TEXT NOT NULL,
+    token 		TEXT NOT NULL,
+    salt 		TEXT NOT NULL,
+    createdAt 	TEXT NOT NULL,
+    UNIQUE(email, token)
+);
+
+CREATE TABLE rooms (
+    user1       TEXT NOT NULL,
+    user2       TEXT NOT NULL,
+    token       TEXT NOT NULL UNIQUE,
+    CHECK(user2 != user1)
+);
+
+CREATE TABLE messages (
+    room        TEXT NOT NULL,
+    isImage     INTEGER NOT NULL,
+    image       TEXT,
+    value       TEXT,
+    file        BLOB,
+    owner       TEXT NOT NULL,
+    time        TEXT NOT NULL
+);
