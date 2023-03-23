0d99948b7072d8f28496d811b30609337fb68f11
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-23 21:11:33 +0300

    User representation added.

---
+++ src/main/kotlin/database/entities/User.kt
@@ -0,0 +1,34 @@
+package database.entities
+
+data class User(
+    val id: Int = 0,
+    val username: String = "",
+    val email: String = "",
+    val token: ByteArray = byteArrayOf(),
+    val salt: ByteArray = byteArrayOf(),
+    val createdAt: String = ""
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (id != other.id) return false
+        if (username != other.username) return false
+        if (email != other.email) return false
+        if (!token.contentEquals(other.token)) return false
+        if (!salt.contentEquals(other.salt)) return false
+        return createdAt == other.createdAt
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + username.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + token.contentHashCode()
+        result = 31 * result + salt.contentHashCode()
+        result = 31 * result + createdAt.hashCode()
+        return result
+    }
+}
