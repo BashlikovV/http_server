54ab7cfc726dd058632b724fe2869250bb3224b6
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-23 21:11:15 +0300

    SQLiteContract added.

---
+++ src/main/kotlin/database/SQLiteContract.kt
@@ -0,0 +1,34 @@
+package database
+
+object SQLiteContract {
+
+    const val MESSENGER_SQLITE_DATABASE_URL = "jdbc:sqlite:messenger.db"
+
+    object UsersTable {
+        const val TABLE_NAME = "users"
+        const val COLUMN_ID = "id"
+        const val COLUMN_USERNAME = "username"
+        const val COLUMN_EMAIL = "email"
+        const val COLUMN_TOKEN = "token"
+        const val COLUMN_SALT = "salt"
+        const val COLUMN_CREATED_AT = "createdAt"
+    }
+
+    object RoomsTable {
+        const val TABLE_NAME = "rooms"
+        const val COLUMN_USER_1 = "user1"
+        const val COLUMN_USER_2 = "user2"
+        const val COLUMN_TOKEN = "token"
+    }
+
+    object MessagesTable {
+        const val TABLE_NAME = "messages"
+        const val COLUMN_ROOM = "room"
+        const val COLUMN_IS_IMAGE = "isImage"
+        const val COLUMN_IMAGE = "image"
+        const val COLUMN_VALUE = "value"
+        const val COLUMN_FILE = "file"
+        const val COLUMN_OWNER = "owner"
+        const val COLUMN_TIME = "time"
+    }
+}
