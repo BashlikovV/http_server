7cc6aa4b6d6a4070a5deb023ae17bf8759be3181
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-09 12:36:46 +0300

    WebSocketHandler added.

---
+++ src/main/kotlin/server/WebSocketHandlerImpl.kt
@@ -0,0 +1,14 @@
+package server
+
+class WebSocketHandlerImpl : WebSocketHandler {
+    override fun handle(request: HttpRequest, response: HttpResponse): ByteArray? {
+        return when(request.url.substringBefore("?").substringAfter("/")) {
+            HttpContract.UrlMethods.GET_IMAGE -> {
+                response.handleGetImageRequest(request = request)
+            }
+            else -> {
+                byteArrayOf()
+            }
+        }
+    }
+}
