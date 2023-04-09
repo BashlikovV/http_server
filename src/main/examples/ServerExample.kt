f6a3bb03cf2e89096c17e51429b80909a50267e3
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-09 12:36:05 +0300

    ServerExample.kt - example of server with async server socket.

---
+++ src/main/examples/ServerExample.kt
@@ -0,0 +1,88 @@
+class Server(
+    private val handler: HttpHandler?
+) {
+
+    private lateinit var server : AsynchronousServerSocketChannel
+
+    companion object {
+        private const val BUFFER_SIZE = 256
+    }
+
+    fun bootstrap() {
+        try {
+            server = AsynchronousServerSocketChannel.open()
+            server.bind(InetSocketAddress(Repository.IP_ADDRESS, Repository.PORT))
+            while (true) {
+                val future = server.accept()
+                handleClient(future)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        } catch (e: ExecutionException) {
+            e.printStackTrace()
+        } catch (e: TimeoutException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun handleClient(future: Future<AsynchronousSocketChannel>){
+        println("New client connection")
+
+        val clientChannel = future.get()
+
+        while (clientChannel != null && clientChannel.isOpen) {
+            val buffer = ByteBuffer.allocate(BUFFER_SIZE)
+            val builder = StringBuilder()
+            var keepReading = true
+
+            while (keepReading) {
+                val readResult = clientChannel.read(buffer).get()
+
+                keepReading = readResult == BUFFER_SIZE
+                buffer.flip()
+                val charBuffer = StandardCharsets.UTF_8.decode(buffer)
+                builder.append(charBuffer)
+
+                buffer.clear()
+            }
+
+            val request = HttpRequest(builder.toString())
+            val response = HttpResponse()
+
+            if (handler != null) {
+                try {
+                    if (request.method == HttpMethod.GET) {
+                        val socket = Socket()
+                        socket.connect(clientChannel.remoteAddress)
+                    } else {
+                        val body = this.handler.handle(request, response)
+
+                        if (!body.isNullOrBlank()) {
+                            if (response.headers["Content-Type"] == null) {
+                                response.addHeader("Content-Type", "application/json; charset=utf-8")
+                            }
+                            response.setBody(body)
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+
+                    response.statusCode = 500
+                    response.status = "Internal server error"
+                    response.addHeader("Content-Type", "application/json; charset=utf-8")
+                }
+            } else {
+                response.statusCode = 404
+                response.status = "Not found"
+                response.addHeader("Content-Type", "application/json; charset=utf-8")
+            }
+
+            val resp = ByteBuffer.wrap(response.getBytes())
+            if (clientChannel.write(resp).get() >= resp.position()) {
+                clientChannel.close()
+            }
+        }
+    }
+}
