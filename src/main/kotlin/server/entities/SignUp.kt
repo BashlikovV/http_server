7f3f206995cf94ac9b9ef235ed6ee65febf09a0f
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-13 13:48:05 +0300

    Image added im signUp request.

--- src/main/kotlin/server/entities/SignUp.kt
+++ src/main/kotlin/server/entities/SignUp.kt
@@ -3,7 +3,8 @@ package server.entities
 data class SignUpRequestBody(
     val username: String,
     val email: String,
-    val password: String
+    val password: String,
+    val image: String
 )
 
 data class SignUpResponseBody(
