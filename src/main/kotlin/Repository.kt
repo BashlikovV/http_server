d76fe61420852585e435945d4c8734f99b4c59d9
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-26 15:12:42 +0300

    Code cleaned.

--- src/main/kotlin/Repository.kt
+++ src/main/kotlin/Repository.kt
@@ -1,8 +1,8 @@
 object Repository {
 
-    const val IP_ADDRESS = "192.168.100.7"
+//    const val IP_ADDRESS = "192.168.100.7"
     const val PORT = 8080
-//    const val IP_ADDRESS = "192.168.194.112"
+    const val IP_ADDRESS = "192.168.52.112"
 
     const val IMAGES_DIRECTORY = "/home/bashlykovvv/IntelliJIDEAProjects/http_server/src/main/resources/images/"
 }
