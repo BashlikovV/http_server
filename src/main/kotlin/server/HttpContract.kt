be12eaa0777182010c21893d5bd8575cc980f225
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-05-04 13:56:51 +0300

    read messages method added.

--- src/main/kotlin/server/HttpContract.kt
+++ src/main/kotlin/server/HttpContract.kt
@@ -16,6 +16,7 @@ object HttpContract {
         const val GET_USER = "get-user"
         const val DELETE_MESSAGE = "delete-message"
         const val UPDATE_USERNAME = "update-username"
+        const val READ_MESSAGES = "read-messages"
 
         const val GET_IMAGE = "get-image"
         const val ADD_IMAGE = "add-image"
