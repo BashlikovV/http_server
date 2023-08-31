30e9f15375e44a8c2f2f51dd5c443f7e1208fe58
bashlikovvv <volodia413314@gmail.com>
2023-08-31 14:38:00 +0300

    code cleaned.

--- src/main/kotlin/Repository.kt
+++ src/main/kotlin/Repository.kt
@@ -1,6 +1,6 @@
 object Repository {
 
-    const val IP_ADDRESS = "192.168.100.8"
+    const val IP_ADDRESS = "192.168.100.6"
     const val PORT = 8080
 //    const val IP_ADDRESS = "192.168.105.112"
 
